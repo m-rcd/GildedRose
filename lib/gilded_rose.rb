6dd6d0851c63046b576e0d5d58935253acd1990b
Marianne Rachid <marianne.rachid@gmail.com>
2018-11-08 09:20:34 +0000

    Added mock item class in test

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,7 +1,6 @@
 class GildedRose
-
   def update_quality(item)
-     if item.name == "Backstage passes to a TAFKAL80ETC concert" && item.quality < 50
+     if item.name ==  'Backstage passes to a TAFKAL80ETC concert' && quality_less_than_max?(item)
        if item.sell_in < 0
          item.quality = 0
        elsif item.sell_in < 6
@@ -13,14 +12,24 @@ class GildedRose
        end
      end
 
-      if item.name != 'Sulfuras, Hand of Ragnaros' && item.name !=  'Backstage passes to a TAFKAL80ETC concert' && item.name != 'Aged Brie'
-        item.sell_in < 0 ? item.quality -= 2 : item.quality -= 1
-      end
+   if regular_item(item)
+     item.sell_in < 0 ? item.quality -= 2 : item.quality -= 1
+   end
 
-      item.quality += 1 if item.name == 'Aged Brie' && item.quality < 50
-    end
+   item.quality += 1 if item.name == 'Aged Brie' && quality_less_than_max?(item)
+ end
 
   def update_sell_in(item)
     item.sell_in -= 1 if item.name != "Sulfuras, Hand of Ragnaros"
+  end
+
+  private
+
+  def quality_less_than_max?(item)
+    item.quality < 50
+  end
+
+  def regular_item(item)
+    true if item.name != 'Sulfuras, Hand of Ragnaros' && item.name !=  'Backstage passes to a TAFKAL80ETC concert' && item.name != 'Aged Brie'
   end
 end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,76 +1,80 @@
 require './lib/gilded_rose'
 
 describe GildedRose do
-
+  subject(:gildedRose) { described_class.new }
+  class MockItem
+    attr_accessor :name, :sell_in, :quality
+    def initialize(name, sell_in, quality)
+      @name = name
+      @sell_in = sell_in
+      @quality = quality
+    end
+  end
   describe "#update_quality" do
-    it "does not change the name" do
-      item = Item.new("foo", 0, 0)
-      GildedRose.new.update_quality(item)
-      expect(item.name).to eq "foo"
-    end
+    context 'Regular item' do
+      it "degrades twice as fast if sell_in date has passed" do
+        item = MockItem.new("foo", -1, 2)
+        gildedRose.update_quality(item)
+        expect(item.quality).to eq 0
+      end
 
-    it "degrades twice as fast if sell_in date has passed" do
-      item = Item.new("foo", -1, 2)
-      GildedRose.new.update_quality(item)
-      expect(item.quality).to eq 0
-    end
-
-    it 'degrades by 1 if it is not sulfuras or backstage passes or aged brie' do
-      item = Item.new("foo", 3, 2)
-      GildedRose.new.update_quality(item)
-      expect(item.quality).to eq(1)
+      it 'degrades by 1 as it gets older' do
+        item = MockItem.new("foo", 3, 2)
+        gildedRose.update_quality(item)
+        expect(item.quality).to eq(1)
+      end
     end
 
     context 'Aged Brie' do
       it 'increases the older it gets' do
-        item = Item.new("Aged Brie", 3, 4)
-        GildedRose.new.update_quality(item)
+        item = MockItem.new("Aged Brie", 3, 4)
+        gildedRose.update_quality(item)
         expect(item.quality).to eq(5)
       end
 
       it "can't increase more than 50" do
-        item = Item.new("Aged Brie", 3, 50)
-        GildedRose.new.update_quality(item)
+        item = MockItem.new("Aged Brie", 3, 50)
+        gildedRose.update_quality(item)
         expect(item.quality).to eq(50)
       end
     end
 
     context 'Sulfuras' do
       it 'cannot decrease in quality if name is sulfuras' do
-        item = Item.new("Sulfuras, Hand of Ragnaros", -1, 80)
-        GildedRose.new.update_quality(item)
+        item = MockItem.new("Sulfuras, Hand of Ragnaros", -1, 80)
+        gildedRose.update_quality(item)
         expect(item.quality).to eq(80)
       end
     end
 
     context 'Backstage passes' do
       it 'increases the older it gets' do
-        item = Item.new("Backstage passes to a TAFKAL80ETC concert", 16, 4)
-        GildedRose.new.update_quality(item)
+        item = MockItem.new("Backstage passes to a TAFKAL80ETC concert", 16, 4)
+        gildedRose.update_quality(item)
         expect(item.quality).to eq(5)
       end
 
       it 'increases by 2 if sell_in < 11' do
-        item = Item.new("Backstage passes to a TAFKAL80ETC concert", 9, 4)
-        GildedRose.new.update_quality(item)
+        item = MockItem.new("Backstage passes to a TAFKAL80ETC concert", 9, 4)
+        gildedRose.update_quality(item)
         expect(item.quality).to eq(6)
       end
 
       it 'increases by 3 if sell_in < 6' do
-        item = Item.new("Backstage passes to a TAFKAL80ETC concert", 4, 4)
-        GildedRose.new.update_quality(item)
+        item = MockItem.new("Backstage passes to a TAFKAL80ETC concert", 4, 4)
+        gildedRose.update_quality(item)
         expect(item.quality).to eq(7)
       end
 
       it 'goes down to 0 if sellin < 0' do
-        item = Item.new("Backstage passes to a TAFKAL80ETC concert", -1, 4)
-        GildedRose.new.update_quality(item)
+        item = MockItem.new("Backstage passes to a TAFKAL80ETC concert", -1, 4)
+        gildedRose.update_quality(item)
         expect(item.quality).to eq(0)
       end
 
       it "can't increase more than 50" do
-        item = Item.new("Backstage passes to a TAFKAL80ETC concert", 3, 50)
-        GildedRose.new.update_quality(item)
+        item = MockItem.new("Backstage passes to a TAFKAL80ETC concert", 3, 50)
+        gildedRose.update_quality(item)
         expect(item.quality).to eq(50)
       end
     end
@@ -78,14 +82,14 @@ describe GildedRose do
 
   describe '#update_sell_in' do
     it 'decreases sell_in' do
-      item = Item.new('foo', 2, 2)
-      GildedRose.new.update_sell_in(item)
+      item = MockItem.new('foo', 2, 2)
+      gildedRose.update_sell_in(item)
       expect(item.sell_in).to eq(1)
     end
 
     it 'does not alter if name is Sulfuras' do
-      item = Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
-      GildedRose.new.update_sell_in(item)
+      item = MockItem.new("Sulfuras, Hand of Ragnaros", 0, 80)
+      gildedRose.update_sell_in(item)
       expect(item.sell_in).to eq(0)
     end
   end
