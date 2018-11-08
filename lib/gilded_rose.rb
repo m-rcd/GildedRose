cb0eae6fef1822b2d2d59951fb8a5e8f6a30850f
Marianne Rachid <marianne.rachid@gmail.com>
2018-11-08 12:16:49 +0000

    fix rubocop offenses

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,16 +9,17 @@ class GildedRose
   end
 
   def update_sell_in(item)
-    item.sell_in -= 1 if !sulfuras?(item)
+    item.sell_in -= 1 unless sulfuras?(item)
   end
 
   def update_regular_item_quality(item)
-    item.sell_in < 0 ? item.quality -= 2 : item.quality -= 1
+    item.sell_in.negative? ? item.quality -= 2 : item.quality -= 1
   end
 
   def update_backstage_quality(item)
-    return item.quality = 0 if item.sell_in < 0
+    return item.quality = 0 if item.sell_in.negative?
     return item.quality += 3 if item.sell_in < 6
+
     return item.sell_in < 11 ?  item.quality += 2 : item.quality += 1
   end
 
@@ -53,7 +54,7 @@ class GildedRose
   end
 
   def regular_item_and_positive?(item)
-    regular_item(item) && item.quality > 0
+    regular_item(item) && item.quality.positive?
   end
 
   def brie_and_less_than_max?(item)
