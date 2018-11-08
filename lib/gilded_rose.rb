8e10ba65a2fd710face9081f1b3bfbc52271f8df
Marianne Rachid <marianne.rachid@gmail.com>
2018-11-08 09:57:49 +0000

    Add private method to check if item backstage,brie,sulfuras

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,30 +1,43 @@
 class GildedRose
+  MAX_quality = 50
+
   def update_quality(item)
-    if item.name ==  'Backstage passes to a TAFKAL80ETC concert' && quality_less_than_max?(item)
-      return item.quality = 0 if item.sell_in < 0
-      return item.quality += 3 if item.sell_in < 6
-      return item.sell_in < 11 ?  item.quality += 2 : item.quality += 1
+    if backstage?(item) && item.quality < MAX_quality
+      update_backstage_quality(item)
     end
 
     if regular_item(item) && item.quality > 0
       item.sell_in < 0 ? item.quality -= 2 : item.quality -= 1
     end
 
-    item.quality += 1 if item.name == 'Aged Brie' && quality_less_than_max?(item)
+    item.quality += 1 if aged_brie?(item) && item.quality < MAX_quality
   end
 
   def update_sell_in(item)
-    item.sell_in -= 1 if item.name != "Sulfuras, Hand of Ragnaros"
+    item.sell_in -= 1 if sulfuras?(item)
   end
 
+  def update_backstage_quality(item)
+    return item.quality = 0 if item.sell_in < 0
+    return item.quality += 3 if item.sell_in < 6
+    return item.sell_in < 11 ?  item.quality += 2 : item.quality += 1
+  end
 
   private
 
-  def quality_less_than_max?(item)
-    item.quality < 50
+  def backstage?(item)
+    item.name == 'Backstage passes to a TAFKAL80ETC concert'
+  end
+
+  def aged_brie?(item)
+    item.name == 'Aged Brie'
+  end
+
+  def sulfuras?(item)
+    item.name == 'Sulfuras, Hand of Ragnaros'
   end
 
   def regular_item(item)
-    true if item.name != 'Sulfuras, Hand of Ragnaros' && item.name !=  'Backstage passes to a TAFKAL80ETC concert' && item.name != 'Aged Brie'
+    true if !sulfuras?(item) && !backstage?(item) && !aged_brie?(item)
   end
 end
